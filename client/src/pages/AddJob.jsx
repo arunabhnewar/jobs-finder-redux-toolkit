83eafc474aec8e30c3227622ca5868168ccab12b
arunabhnewar <newardevil@gmail.com>
2023-04-10 20:07:39 +0600

    add new job done

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,21 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createJob } from "../features/Jobs/jobsSlice";
 
 const AddJob = () => {
+  // use selector
+  const { isLoading, isError } = useSelector(state => state.jobs);
+
+  // dispatch
+  const dispatch = useDispatch();
+
+  // useState
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
+  const [salary, setSalary] = useState("");
+  const [deadline, setDeadline] = useState("");
+
+  // add a new job
+  const addJobHandler = e => {
+    e.preventDefault();
+    dispatch(
+      createJob({
+        title,
+        type,
+        salary: Number(salary),
+        deadline,
+      })
+    );
+    resetHandler();
+  };
+
+  // reset handler
+  const resetHandler = () => {
+    setTitle("");
+    setType("");
+    setSalary("");
+    setDeadline("");
+  };
+
+  // onSubmit={addNewJobHandler}
+
   return (
     <div className='lg:pl-[14rem] mt-[5.8125rem]'>
       <main className='max-w-3xl rounded-lg mx-auto relative z-20 p-10 xl:max-w-none bg-[#1E293B]'>
         <h1 className='mb-10 text-center lws-section-title'>Add New Job</h1>
 
         <div className='max-w-3xl mx-auto'>
-          <form className='space-y-6'>
+          <form onSubmit={addJobHandler} className='space-y-6'>
             <div className='fieldContainer'>
               <label
-                for='lws-JobTitle'
+                htmlFor='lws-JobTitle'
                 className='text-sm font-medium text-slate-300'>
                 Job Title
               </label>
-              <select id='lws-JobTitle' name='lwsJobTitle' required>
-                <option value='' hidden selected>
+              <select
+                id='lws-JobTitle'
+                name='lwsJobTitle'
+                required
+                type='text'
+                value={title}
+                onChange={e => setTitle(e.target.value)}>
+                <option hidden selected>
                   Select Job
                 </option>
                 <option>Software Engineer</option>
@@ -36,9 +80,15 @@ const AddJob = () => {
             </div>
 
             <div className='fieldContainer'>
-              <label for='lws-JobType'>Job Type</label>
-              <select id='lws-JobType' name='lwsJobType' required>
-                <option value='' hidden selected>
+              <label htmlFor='lws-JobType'>Job Type</label>
+              <select
+                id='lws-JobType'
+                name='lwsJobType'
+                required
+                type='text'
+                value={type}
+                onChange={e => setType(e.target.value)}>
+                <option hidden selected>
                   Select Job Type
                 </option>
                 <option>Full Time</option>
@@ -48,7 +98,7 @@ const AddJob = () => {
             </div>
 
             <div className='fieldContainer'>
-              <label for='lws-JobSalary'>Salary</label>
+              <label htmlFor='lws-JobSalary'>Salary</label>
               <div className='flex border rounded-md shadow-sm border-slate-600'>
                 <span className='input-tag'>BDT</span>
                 <input
@@ -58,17 +108,21 @@ const AddJob = () => {
                   required
                   className='!rounded-l-none !border-0'
                   placeholder='20,00,000'
+                  value={salary}
+                  onChange={e => setSalary(e.target.value)}
                 />
               </div>
             </div>
 
             <div className='fieldContainer'>
-              <label for='lws-JobDeadline'>Deadline</label>
+              <label htmlFor='lws-JobDeadline'>Deadline</label>
               <input
                 type='date'
                 name='lwsJobDeadline'
                 id='lws-JobDeadline'
                 required
+                value={deadline}
+                onChange={e => setDeadline(e.target.value)}
               />
             </div>
 
@@ -76,10 +130,15 @@ const AddJob = () => {
               <button
                 type='submit'
                 id='lws-submit'
+                disabled={isLoading}
                 className='cursor-pointer btn btn-primary w-fit'>
                 Submit
               </button>
             </div>
+
+            {!isLoading && isError && (
+              <p className='error'>There was an error occurred</p>
+            )}
           </form>
         </div>
       </main>
